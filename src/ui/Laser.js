5fe3128a8708101e8070f921e6b75bacd0423e28
devillies <devilliesdev@gmail.com>
2024-02-15 18:08:42 +0800

    final game

---
+++ src/ui/Laser.js
@@ -0,0 +1,25 @@
+import { Physics } from 'phaser'
+
+export default class Laser extends Physics.Arcade.Sprite {
+	constructor(scene, x, y, texture) {
+		super(scene, x, y, texture)
+		this.setScale(2)
+	}
+
+	fire(x, y) {
+		this.setPosition(x, y - 50)
+		this.setActive(true)
+		this.setVisible(true)
+	}
+
+	die() {
+		this.destroy()
+	}
+
+	update() {
+		this.setVelocityY(-200)
+		if (this.y < -10) {
+			this.die()
+		}
+	}
+}
